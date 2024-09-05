8c97e1d147eeb720e774624616da9f9133023a95
Sanjeev122 <loadd8124@gmail.com>
2024-09-05 13:48:24 +0530

    Sanjeev is checking tc1

---
+++ src/org/sanjeev/Sanjeev.java
@@ -0,0 +1,13 @@
+package org.sanjeev;
+
+public class Sanjeev {
+	private void tc1() {
+		System.out.println("TC1");
+	}
+	
+
+	public static void main(String[] args) {
+		Sanjeev s=new Sanjeev();
+		s.tc1();
+	}
+}
